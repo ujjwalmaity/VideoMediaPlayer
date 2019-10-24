e706cf6568928a86a8dda405e5b2cf7d213dec0c
ujjwal <ujjwalmaity98@gmail.com>
2019-10-24 12:52:18 +0530

    Add VideoView and play a video

--- app/src/main/java/dev/ujjwal/videomediaplayer/MainActivity.java
+++ app/src/main/java/dev/ujjwal/videomediaplayer/MainActivity.java
@@ -3,12 +3,24 @@ package dev.ujjwal.videomediaplayer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
+
+    VideoView videoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+
+        videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.mere_haath_mein);
+        videoView.start();
+    }
+
+    private void init() {
+        videoView = findViewById(R.id.videoView);
     }
 }
