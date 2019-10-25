f3c09953309425ab2e747056e0662b43be5c14b9
ujjwal <ujjwalmaity98@gmail.com>
2019-10-25 14:29:56 +0530

    Add custom controller i.e. seek, play, pause, replay, stop

--- app/src/main/java/dev/ujjwal/videomediaplayer/MainActivity.java
+++ app/src/main/java/dev/ujjwal/videomediaplayer/MainActivity.java
@@ -3,14 +3,22 @@ package dev.ujjwal.videomediaplayer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.MediaController;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
 import android.widget.VideoView;
+import android.media.MediaPlayer;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     VideoView videoView;
 
-    MediaController mediaController;
+    Button play_pause, replay, stop;
+
+    SeekBar seekBar;
+    Runnable runnable;
+    Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,15 +28,78 @@ public class MainActivity extends AppCompatActivity {
         init();
 
         videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.mere_haath_mein);
-        videoView.start();
 
-        mediaController.setAnchorView(videoView);
-        videoView.setMediaController(mediaController);
+        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                seekBar.setMax(videoView.getDuration());
+            }
+        });
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (fromUser) {
+                    videoView.seekTo(progress);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+
+        play_pause.setOnClickListener(this);
+        replay.setOnClickListener(this);
+        stop.setOnClickListener(this);
     }
 
     private void init() {
         videoView = findViewById(R.id.videoView);
 
-        mediaController = new MediaController(this);
+        play_pause = findViewById(R.id.play_pause);
+        replay = findViewById(R.id.replay);
+        stop = findViewById(R.id.stop);
+
+        seekBar = findViewById(R.id.seekBar);
+        handler = new Handler();
+    }
+
+    private void changeSeekBar() {
+        seekBar.setProgress(videoView.getCurrentPosition());
+
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                changeSeekBar();
+            }
+        };
+        handler.postDelayed(runnable, 100);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.play_pause:
+                if (videoView.isPlaying()) {
+                    videoView.pause();
+                    play_pause.setText("PLAY");
+                    break;
+                }
+                videoView.start();
+                play_pause.setText("PAUSE");
+                changeSeekBar();
+                break;
+            case R.id.replay:
+                videoView.resume();
+                break;
+            case R.id.stop:
+                videoView.suspend();
+                break;
+        }
     }
 }
